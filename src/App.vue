6875f752217ad222c16ab108f45df95dd4f7b1b0
ass315 <ass315@qq.com>
2019-04-23 10:26:36 +0800

    “ 增加临时公共页头，方便随时查看各个页面。 添加说明如下： 每个人的页面再router里增加了路由之后，在App.vue文件里 script 里增加路由设置。 template 里增加自己页面的地址和跳转。 完毕 ”

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div id="app">
-    <HomePage/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/coupon">Coupon</router-link>|
+      <router-link to="/Account">Account</router-link>|
+      <router-link to="/aboutUs">aboutUs</router-link>|
+      <router-link to="/home">home</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HomePage from './components/homepage/index'
-
-export default {
-  name: 'app',
-  components: {
-    HomePage
-  }
-}
-</script>
-
 <style lang="scss">
-
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
