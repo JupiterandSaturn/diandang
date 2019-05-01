565effb2488b12de34574978e7ea5a1da498daa7
ass315 <ass315@qq.com>
2019-05-01 10:08:58 +0800

    改变elementUI主题配色为灰色

--- src/store/OrderInformation/index.js
+++ src/store/OrderInformation/index.js
@@ -3,43 +3,6 @@ import bus from '@/bus'
 export default({
     state:{
         setOrder:[],
-<<<<<<< HEAD
-        getProvinceList:[],
-        getCityList:[],
-    },
-    mutations:{
-        SET_PROVINCE(state,obj){
-            state.getProvinceList = obj;
-        },
-        SET_CITY(state,obj){
-            state.getCityList = obj;
-        }
-    },
-    actions:{
-        setOrder(){
-            bus.$axios.post("/user/user/addUserInfo")
-                .then(({data})=>{
-                    this.state.order = data.code;
-
-                    // console.log(this.state.order);
-                })
-        },
-        getProvinceList({commit}){
-            bus.$axios.post("user/address/provinceList")
-                .then(({data})=>{
-                    // console.log('进入getProvinceList')
-                    this.state.getProvinceList = data.province;
-                    // console.log(data.province);
-                    commit("SET_PROVINCE",data.province)
-                })
-        },
-        getCityList({commit},url){
-            bus.$axios.post("user/address/city/")
-                .then(({data})=>{
-                    this.state.getCityList = data.data;
-                    console.log(this.state.getCityList);
-                    commit("SET_CITY",data);
-=======
         Province:[],
         City:[],
         Area:[],
@@ -100,7 +63,6 @@ export default({
                     this.state.Area = data.area;
                     console.log(data.area);
                     commit("SET_AREA",data.area);
->>>>>>> 20668755e494c806b4864034dc634c898f1aa306
                 })
         }
 
--- src/store/second/index.js
+++ src/store/second/index.js
@@ -25,34 +25,3 @@ export default({
     
     
 })
-<<<<<<< HEAD
-// getTop(){
-//     bus.$axios.post('./pawn/type/header')
-//     .then(({data})=>{
-//         this.state.typename=data.data
-//         console.log(this.state.typename)
-//     })
-// }
-
-// getTop(){
-//     bus.$axios.poat('./pawn/type/header')
-//     .then(({data})=>{
-//         this.state.typename=data.data
-        
-//     })
-// }
-// getTop(){
-//     this.$axios.post('./pawn/type/header')
-//     .then(({data})=>{
-//         this.state.typename=data.data
-//     })
-// }
-// getTop(){
-//     bus.$axios.post('./pawn/type/header')
-//     .then(({data})=>{
-
-//     })
-// }
-=======
-
->>>>>>> 20668755e494c806b4864034dc634c898f1aa306
